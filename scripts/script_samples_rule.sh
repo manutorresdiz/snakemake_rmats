70fd642603e3d9af70ffa24b2b7d90d48e2ab488
Torres Diz <torresdizm@reslnvvhpc025.research.chop.edu>
2021-04-08 10:38:35 -0400

    remove softlink command from script

--- scripts/script_samples_rule.sh
+++ scripts/script_samples_rule.sh
@@ -9,14 +9,6 @@ do
         P2=$(cat $1 | awk "/$patient/ {print}" | awk '{print $3}')
         R1=$(cat $1 | awk "/$patient/ {print}" | awk '{print $4}')
         R2=$(cat $1 | awk "/$patient/ {print}" | awk '{print $5}')
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${P1}/star/${P1}.bam $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${P1}/star/${P1}.bam.bai $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${P2}/star/${P2}.bam $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${P2}/star/${P2.bam.bai $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${R1}/star/${R1}.bam $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${R1}/star/${R1}.bam.bai $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${R2}/star/${R2}.bam $1data/bam_files/
-        ln -s /mnt/isilon/thomas-tikhonenko_lab/target/processed_reads/${R2}/star/${R2.bam.bai $1data/bam_files/
         
         echo "$1data/bam_files/${P1}.bam,$1data/bam_files/${P2}.bam" > $1metadata/${patient}_Primary.txt
         echo "$1data/bam_files/${R1}.bam,$1data/bam_files/${R2}.bam" > $1metadata/${patient}_Relapse.txt
